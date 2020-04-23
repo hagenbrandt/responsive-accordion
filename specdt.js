a985be63b732c1a4a09f3a66e900318e0257aec1
hagenbrandt <hb.hagen.brandt@gmail.com>
2020-04-23 15:16:11 +0200

    "Split content and make it dynamic"

---
+++ specdt.js
@@ -0,0 +1,17 @@
+export const SPECDT = [
+  {
+    group: 'General',
+    items: [
+      { name: 'Model', value: 'DC11' },
+      { name: 'Delivery time', value: '2-3 weeks' },
+    ],
+  },
+  {
+    group: 'Engine',
+    items: [{ name: 'bhp', value: '404' }],
+  },
+  {
+    group: 'specials',
+    text: 'convertible, leather seats',
+  },
+]
