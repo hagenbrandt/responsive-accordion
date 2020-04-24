35197cfddb6bed0b5de8ca88c21b52c629efe204
hagenbrandt <hb.hagen.brandt@gmail.com>
2020-04-24 02:11:13 +0200

    WIP Sass

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ let i = 0
 $.each(SPECDT, function () {
   $('#spec')
     .append(
-      `<h2 class=${this.group} tabindex=${i++}><a class='header' href=#${
+      `<h2 class=${this.group} tabindex=${i++}><a class='header-link' href=#${
         i - 1
       } >${this.group}</a></h2>`
     )
-    .append(`<dl class='accordion' id=${i - 1} index=${i - 1}></dl>`)
+    .append(`<dl class='container' id=${i - 1} index=${i - 1}></dl>`)
   $.each(this.items, function () {
     $(`[index=${i - 1}]`)
       .append(`<dt>${this.name}</dt>`)
@@ -19,4 +19,13 @@ $.each(SPECDT, function () {
   $('[index=2]').append(`<span>${this.text}</span>`)
 })
 
+// $('h2').addClass('header')
+
 accordion()
+
+// $('h2').delegate('*', 'focus blur', function () {
+//   var elem = $(this)
+//   setTimeout(function () {
+//     elem.toggleClass('focused', elem.is(':focus'))
+//   }, 0)
+// })
