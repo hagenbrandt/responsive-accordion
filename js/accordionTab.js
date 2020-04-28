c63577cb246790874805644f8de0dd59d283a9cb
hagenbrandt <hagen-brandt@web.de>
2020-04-28 16:40:27 +0200

    Change DOM, make it responsive and style all elements

--- js/accordionTab.js
+++ js/accordionTab.js
@@ -1,20 +1,34 @@
 export function accordion() {
-  $(document).ready(function ($) {
-    const panels = $('#spec > dl ').hide()
-    panels.first().show()
 
-    $('#spec > h2 > a').click(function () {
-      var $this = $(this)
+  $('.container').hide()
+  $('.container:first').show()
 
-      if (document.documentElement.clientWidth < 768) {
-        panels.slideUp()
-        $this.parent().next().slideDown()
-      } else {
-        panels.hide()
-        $this.parent().next().show()
-      }
+  $("ul.tabs li").click(function() {
+    $(".container").hide();
 
-      return false
-    })
+      const activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn(500);
+
+      $("ul.tabs li").removeClass("active");
+      $(this).addClass("active");
+
+      $(".tab_drawer_heading").removeClass("d_active");
+
+      $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
   })
+
+  $(".tab_drawer_heading").click(function() {
+      
+    $(".container").hide();
+    const d_activeTab = $(this).attr("rel"); 
+    $("#"+d_activeTab).fadeIn();
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+
+    $("ul.tabs li").removeClass("active");
+    $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+  });
+
+  $('ul.tabs li').last().addClass("tab_last");
 }
--- js/index.js
+++ js/index.js
@@ -3,29 +3,24 @@ import { accordion } from './accordionTab'
 
 let i = 0
 
+$('#spec').append('<ul class="tabs"></ul>').append("<div class='tab_container'></div>")
 $.each(SPECDT, function () {
-  $('#spec')
-    .append(
-      `<h2 class=${this.group} tabindex=${i++}><a class='header-link' href=#${
-        i - 1
-      } >${this.group}</a></h2>`
+
+    $('ul').append(
+      `<li tabindex=${i++} rel=tabs-${i-1}>${this.group}</li>`
     )
-    .append(`<dl class='container' id=${i - 1} index=${i - 1}></dl>`)
+
+    $('.tab_container')
+    .append(`<h3 class="d_active tab_drawer_heading" container_id=${i-1} rel=tabs-${i-1}>${this.group}</h3>`)
+    .append(`<dl class='container' id=tabs-${i - 1} index=${i - 1}></dl>`)
+
   $.each(this.items, function () {
     $(`[index=${i - 1}]`)
       .append(`<dt>${this.name}</dt>`)
       .append(`<dd>${this.value}</dd>`)
   })
+
   $('[index=2]').append(`<span>${this.text}</span>`)
 })
 
-// $('h2').addClass('header')
-
 accordion()
-
-// $('h2').delegate('*', 'focus blur', function () {
-//   var elem = $(this)
-//   setTimeout(function () {
-//     elem.toggleClass('focused', elem.is(':focus'))
-//   }, 0)
-// })
